50b1120a9999dca25fe25579852f348814a2cb75
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2023-09-24 16:44:37 -0700

    feat(locals)

--- locals.tf
+++ locals.tf
@@ -15,5 +15,13 @@ locals {
       topics             = ["frontend-masters", "terraform"]
       visibility         = "private"
     }
+
+    "fem-eci-terraform-product-service" = {
+      description        = "Automation for product services"
+      gitignore_template = "Terraform"
+      name               = "fem-eci-terraform-product-service"
+      topics             = ["frontend-masters", "terraform"]
+      visibility         = "private"
+    }
   }
 }
